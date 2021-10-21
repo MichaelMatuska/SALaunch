dd2a0375e054a9cf06fa3e852e51ee4696e9ae37
Matuska <pmatuska@amazon.com>
2021-10-21 13:05:00 -0400

    Update v2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Team Mobile!</h1>
+        <h1>Team Mobile V2!</h1>
       </header>
       <AmplifySignOut />
     </div>
